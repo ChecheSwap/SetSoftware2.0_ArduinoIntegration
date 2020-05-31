83a1d95b0f2ed13ddb3af64454e571ca2e0fb89b
Jesús José Navarrete Baca <chechenb19972@hotmail.com>
2020-05-31 01:29:42 -0600

    Add files via upload

---
+++ viewdetallesticket.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE VIEW VIEWDETALLESTICKET AS 
+
+SELECT CONVERT(SUM(A.CANTIDAD),CHAR) AS CANTIDAD, 
+
+		(CASE WHEN A.DESCUENTO > 0 
+
+		THEN 
+		
+			CONCAT(B.NOMBRE,'-',A.DESCUENTO,'%') 
+		ELSE 
+			B.NOMBRE 
+		
+		END ) AS NOMBRE, 
+			
+            CONVERT(A.PRECIO,CHAR) AS PRECIO, CONVERT(SUM(A.CANTIDAD * A.PRECIO)-((SUM(A.CANTIDAD * A.PRECIO)/100)*A.DESCUENTO),CHAR) AS IMPORTE    
+			
+            FROM DETALLE A INNER JOIN PRODUCTO B
+					
+                    ON A.PKPRODUCTO = B.PKPRODUCTO   				
+						
+                        GROUP BY  A.IDVENTA,  A.PKPRODUCTO, A.DESCUENTO;
+                                               
+                        				
+					
+                        
+                        
+              
