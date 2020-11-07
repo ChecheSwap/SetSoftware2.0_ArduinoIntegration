466e3d1ed429fb05fc84a2258f26018837c435d7
Jesús José Navarrete Baca <chechenb19972@hotmail.com>
2020-11-07 15:31:51 -0700

    Update spgetdetallesventa.sql

--- DBO/spgetdetallesventa.sql
+++ DBO/spgetdetallesventa.sql
@@ -1,6 +1,12 @@
 
---JESUS JOSE NAVARRETE BACA @CHECHESWAP
---NOVEMBER 2019
+ /*---------------------------------------------------------------------
+  -                                                                    -
+  -               Stored Procedure spgetdetallesticket DDL             -
+  -                           MySQL RDBMS                              -
+  -             Ing. Jesús José Navarrete Baca (@checheswap)           -
+  -                            November 2019                           -
+  -                                                                    -
+  ---------------------------------------------------------------------*/
 
 DROP PROCEDURE IF EXISTS spgetdetallesticket;
 DELIMITER //
@@ -8,10 +14,12 @@ CREATE PROCEDURE spgetdetallesticket(IN NUMEROVENTA INT)
 BEGIN
 SELECT SUM(A.CANTIDAD) AS CANTIDAD, (CASE WHEN A.DESCUENTO > 0 THEN CONCAT(B.NOMBRE,'-',A.DESCUENTO,'%') ELSE B.NOMBRE END ) AS NOMBRE, 
        A.PRECIO AS PRECIO, SUM(A.CANTIDAD * A.PRECIO)-((SUM(A.CANTIDAD * A.PRECIO)/100)*A.DESCUENTO)AS IMPORTE    
-FROM  DETALLE A INNER JOIN PRODUCTO B
-ON    A.PKPRODUCTO = B.PKPRODUCTO   
-WHERE A.IDVENTA    = NUMEROVENTA
-GROUP BY A.PKPRODUCTO, A.DESCUENTO;
+FROM   DETALLE A 
+INNER 
+JOIN   PRODUCTO B
+ON     A.PKPRODUCTO = B.PKPRODUCTO   
+WHERE  A.IDVENTA    = NUMEROVENTA
+GROUP  BY A.PKPRODUCTO, A.DESCUENTO;
 END
 //
 
