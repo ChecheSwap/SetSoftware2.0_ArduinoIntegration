acfb089cdc997a037207e698567cd1d62a24f42c
Jesús José Navarrete Baca <chechenb19972@hotmail.com>
2020-11-07 15:35:18 -0700

    Update viewdetallesticket.sql

--- DBO/viewdetallesticket.sql
+++ DBO/viewdetallesticket.sql
@@ -1,27 +1,25 @@
---JESUS JOSE NAVARRETE BACA @CHECHESWAP
---NOVEMBER 2019
+ /*---------------------------------------------------------------------
+  -                                                                    -
+  -                    View viewDetallesTicket DDL                     -
+  -                           MySQL RDBMS                              -
+  -             Ing. Jesús José Navarrete Baca (@checheswap)           -
+  -                            November 2019                           -
+  -                                                                    -
+  ---------------------------------------------------------------------*/
+CREATE OR REPLACE VIEW VIEWDETALLESTICKET 
+AS 
 
-CREATE OR REPLACE VIEW VIEWDETALLESTICKET AS 
-
-SELECT CONVERT(SUM(A.CANTIDAD),CHAR) AS CANTIDAD, 
-
-		(CASE WHEN A.DESCUENTO > 0 
-
-		THEN 
-		
-			CONCAT(B.NOMBRE,'-',A.DESCUENTO,'%') 
-		ELSE 
-			B.NOMBRE 
-		
-		END ) AS NOMBRE, 
-			
-            CONVERT(A.PRECIO,CHAR) AS PRECIO, CONVERT(SUM(A.CANTIDAD * A.PRECIO)-((SUM(A.CANTIDAD * A.PRECIO)/100)*A.DESCUENTO),CHAR) AS IMPORTE    
-			
-            FROM DETALLE A INNER JOIN PRODUCTO B
-					
-                    ON A.PKPRODUCTO = B.PKPRODUCTO   				
-						
-                        GROUP BY  A.IDVENTA,  A.PKPRODUCTO, A.DESCUENTO;
+SELECT    CONVERT(SUM(A.CANTIDAD),CHAR) AS CANTIDAD,(CASE WHEN A.DESCUENTO > 0 THEN 
+		                                    CONCAT(B.NOMBRE,'-',A.DESCUENTO,'%') 
+						  ELSE 
+						    B.NOMBRE 		
+						  END) AS NOMBRE, 			
+          CONVERT(A.PRECIO,CHAR) AS PRECIO, CONVERT(SUM(A.CANTIDAD * A.PRECIO)-((SUM(A.CANTIDAD * A.PRECIO)/100)*A.DESCUENTO),CHAR) AS IMPORTE    			
+FROM      DETALLE A 
+INNER 
+JOIN      PRODUCTO B				
+ON        A.PKPRODUCTO = B.PKPRODUCTO   										
+GROUP BY  A.IDVENTA,  A.PKPRODUCTO, A.DESCUENTO;
                                                
                         				
 					
